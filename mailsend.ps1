c352ff37451cecf163001945a0dc66e2518c057e
Sguinther <65420134+Sguinther@users.noreply.github.com>
2021-10-27 19:37:35 -0700

    Add files via upload

---
+++ mailsend.ps1
@@ -0,0 +1,19 @@
+﻿#storyline send an email
+
+# body of email
+
+$msg = "hello!"
+
+write-host -BackgroundColor Red -ForegroundColor white $msg
+
+$email = "sam.guinther@mymail.champlain.edu" 
+
+
+#to address
+
+$toemail = "deployer@csi-web"
+
+#send mail
+Send-MailMessage -From $email -to $toemail -Subject "Oi what it is" -body $msg -SmtpServer 192.168.6.71
+
+
---
+++ securitylog.ps1
@@ -0,0 +1,20 @@
+﻿#storyline review security event log
+
+$myDir = "C:\Users\champuser\Desktop\"
+
+#list event logs
+Get-EventLog -list
+
+#get keyword
+$key = Read-host -Prompt "select keyword to search for"
+
+#create a promt to allow user to selecxt the log to view
+$readlog = Read-host -Prompt "Please select a log to review from the list above" 
+
+#print the results for the log
+Get-EventLog -LogName $readlog -Newest 40 | where {$_.Message -ilike $key } | export-csv -NoTypeInformation 
+-Path "$myDir\securityLogs.csv"
+
+
+
+
